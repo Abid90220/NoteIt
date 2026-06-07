eb666f80145d7caa9da5a9a132fcc76a66c5a84f
Abid90220 <dealwithsami@gmail.com>
2026-06-07 15:02:16 -0500

    Deploy frontend to GitHub Pages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,7 @@ import {Toaster} from "react-hot-toast";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
       <Toaster />
     </BrowserRouter>
